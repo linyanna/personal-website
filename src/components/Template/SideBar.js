9f1970b4d5fa423dafa69c3baafc01b4c9b2edda
Yanna Lin <linyanna@yahoo.com>
2023-03-07 03:31:29 -0500

    updated with example's code

---
+++ src/components/Template/SideBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ContactIcons from '../Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const SideBar = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Yanna Lin</h2>
+        <p><a href="mailto:linyanna@yahoo.com">linyanna@yahoo.com</a></p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>Hi, I&apos;m Yanna. I like building things.
+        I am a <a href="https://icme.stanford.edu/">University of Florida</a> student and researcher at <a href="https://www.cise.ufl.edu/~eragan/indie.html">INDIE Lab</a>.
+      </p>
+      <ul className="actions">
+        <li>
+          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ContactIcons />
+      <p className="copyright">&copy; Yanna Lin <Link to="/">yannalin.dev</Link>.</p>
+    </section>
+  </section>
+);
+
+export default SideBar;
---
+++ src/data/resume/degrees.js
@@ -0,0 +1,10 @@
+const degrees = [
+  {
+    school: 'University of Florida',
+    degree: 'B.S. Computer Science',
+    link: 'https://ufl.edu',
+    year: 2024,
+  },
+];
+
+export default degrees;
